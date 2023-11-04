c34dd22c8dd38df1c296208631f0ea1dd1d36fa6
Daniel Spofford <868014+danielspofford@users.noreply.github.com>
2023-11-03 23:13:12 -0600

    fix limit, order, and page params

--- src/api/binaries.rs
+++ src/api/binaries.rs
@@ -143,13 +143,20 @@ impl<'a> BinariesApi<'a> {
         &'a self,
         params: ListBinariesParams,
     ) -> Result<Option<ListBinariesResponse>, Error> {
-        let search_string = params.search;
+        let mut query_params = vec![("search".to_string(), params.search)];
+
+        if let Some(limit) = params.limit {
+            query_params.push(("limit".to_string(), limit.to_string()))
+        }
+        if let Some(order) = params.order {
+            query_params.push(("order".to_string(), order))
+        }
+
+        if let Some(page) = params.page {
+            query_params.push(("page".to_string(), page))
+        }
         self.0
-            .execute(
-                Method::GET,
-                format!("/binaries?search={search_string}"),
-                None,
-            )
+            .execute_with_params(Method::GET, "/binaries".to_string(), None, query_params)
             .await
     }
 
